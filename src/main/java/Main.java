d4d1f341383654c85eb5fac21e471bb69e0d0bab
Arsenii <vatrushka.96@mail.ru>
2023-06-11 16:14:15 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
+@SpringBootApplication(scanBasePackages = "MerchantSolutions", exclude = { SecurityAutoConfiguration.class , ErrorMvcAutoConfiguration.class})
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
