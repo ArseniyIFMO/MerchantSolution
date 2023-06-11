e671f2da3c20dceb78883367fcb7f32f53b2fcfc
Arsenii <vatrushka.96@mail.ru>
2023-06-11 17:25:07 +0200

    Add default signal test

--- src/test/java/Signals/DefaultSignalTest.java
+++ src/test/java/Signals/DefaultSignalTest.java
@@ -7,19 +7,17 @@ import org.mockito.InOrder;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
 
-public class Signal2Test {
+public class DefaultSignalTest {
 
     @Test
     void testExecuteSignal2() {
         Algo algo = mock(Algo.class);
-        Signal2 signal2 = new Signal2();
+        DefaultSignal signal2 = new DefaultSignal();
         signal2.executeSignal(algo);
 
         InOrder inOrder = inOrder(algo);
 
-        inOrder.verify(algo).reverse();
-        inOrder.verify(algo).setAlgoParam(1, 80);
-        inOrder.verify(algo).submitToMarket();
+        inOrder.verify(algo).cancelTrades();
         inOrder.verify(algo).doAlgo();
 
     }
